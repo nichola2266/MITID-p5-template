54e953bbbf169995bd224aee0779009ba2982ccd
Thomas Perivolas <thomas.perivolas@gmail.com>
2025-10-27 18:57:44 +0100

    first commit

---
+++ hello-p5/script.js
@@ -0,0 +1,15 @@
+// 👉🏻 https://p5js.org/reference/
+
+
+
+function setup() {
+    // setup run once
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    // draw run repeatedly
+    background(200);
+
+}
